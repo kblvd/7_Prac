7035a72703754de1da2bd379410b715ad0dbb1c7
kblvd <koblovdmitry1@gmail.com>
2022-05-31 22:30:04 +0400

    add

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult BorowsGuests()
+        {
+            GuestContext db = new GuestContext();
+            return View(db.GuestResponse);
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse gt)
+        {
+            GuestContext db = new GuestContext();
+            if (ModelState.IsValid)
+            {
+                db.GuestResponse.Add(gt);
+                db.SaveChanges();
+                return View("Thanks", gt);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace PartyInvites.Models
+{
+    public class GuestContext : DbContext
+    {
+        public DbSet<GuestResponse> GuestResponse { get; set; }
+    }
+
+    public class GuestResponse
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите свое имя")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите свой адресс")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный адресс")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите телефон")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, укажите, примите ли участие в вечеринке")]
+        public string Attend { get; set; }
+    }
+}
